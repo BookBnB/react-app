8e28c7a7e76d4a9e3823c6f70ea37e5436899d81
Franco Mazzoni <franco.mazzoni@despegar.com>
2020-10-19 20:20:46 -0300

    Update

--- docker/heroku_push.sh
+++ docker/heroku_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./docker/build_production_image.sh
-docker tag bookbnb-react:latest registry.heroku.com/bookbnb-react-master/web
+docker tag bookbnb-react:latest registry.heroku.com/$HEROKU_APP_NAME-$TRAVIS_BRANCH/web
 echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.heroku.com
-docker push registry.heroku.com/nameless-springs-01824/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
 curl https://cli-assets.heroku.com/install.sh | sh  #install heroku
-heroku container:release web -a bookbnb-react
+heroku container:release web -a $HEROKU_APP_NAME
