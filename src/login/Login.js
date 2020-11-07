39805b1746d5db53d1d5a44434c9d14747a8e334
Franco Mazzoni <franco.mazzoni@despegar.com>
2020-11-07 14:35:44 -0300

    Add validations

--- src/login/Login.js
+++ src/login/Login.js
@@ -3,36 +3,70 @@ import './login.css';
 
 function Login() {
 
-    const [response, setResponse] = useState(null);
+    const [mail, setMail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-    function handleClick() {
+    function validateFieldsAndLogin() {
+        if (mail === '') {
+            setErrorMessage('El mail no puede estar vacío')
+        } else if (password === '') {
+            setErrorMessage('La contraseña no puede estar vacía')
+        } else {
+            login();
+        }
+    }
 
-        fetch(process.env.REACT_APP_BACKEND_URL + '/v1/users', {
-            method: 'GET',
+    function login() {
+
+        fetch(process.env.REACT_APP_BACKEND_URL + '/v1/sesiones', {
+            method: 'POST',
+            body: JSON.stringify({email: mail, password: password}),
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             },
         })
-            .then((res) => res.json())
-            .catch((error) => console.error('Error:', error))
-            .then((r) => {
-                setResponse(r);
-            });
+        .then((res) => res.json())
+        .catch((error) => console.error('Error:', error))
+        .then((response) => {
+            console.log(response);
+            if (response.message) {
+                setErrorMessage(response.message)
+            } else {
+                loginSuccesful();
+            }
+        });
 
+    }
+
+    function loginSuccesful() {
+        console.alert("Login exitoso");
+        this.props.history.push("/home");
+    }
+
+    function handleMailChange(event) {
+        setMail(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
     }
 
     return (
         <div className='login'>
-            <form className="login-form">
-                <label htmlFor="mail">Mail</label>
-                <input id="mail" name="mail" type="text"/>
+            <div className="mail">
+                <div>Mail</div>
+                <input id="mail" name="mail" type="text" onChange={handleMailChange}/>
+            </div>
 
-                <label htmlFor="password">Contraseña</label>
-                <input id="password" name="password" type="text"/>
+            <div className="password">
+                <div>Contraseña</div>
+                <input id="password" name="password" type="password" onChange={handlePasswordChange}/>
+            </div>
 
-                <button onClick={handleClick}>Ingresar</button>
-            </form>
+            <button onClick={validateFieldsAndLogin}>Ingresar</button>
+            <div className="error-message">{errorMessage}</div>
         </div>
     );
 
