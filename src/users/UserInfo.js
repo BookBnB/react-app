ece5c6f06d6d6fbfcb572a81cd4c6cc8fc9fd1e7
Franco Mazzoni <franco.mazzoni73@gmail.com>
2021-01-27 15:30:02 -0300

    Add modals for user options

--- src/users/UserInfo.js
+++ src/users/UserInfo.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import './userInfo.css';
 import Link from "@material-ui/core/Link";
+import Modal from "@material-ui/core/Modal";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField/TextField";
 
 export default function UserInfo({user}) {
+
+    const [showProfileModal, setShowProfileModal] = useState(false);
+    const [showBlockUserConfirmationModal, setShowBlockUserConfirmationModal] = useState(false);
+    const [showChargeModal, setShowChargeModal] = useState(false);
+    const [amountToCharge, setAmountToCharge] = useState(0);
+
+    const openProfileModal = () => {
+      setShowProfileModal(true);
+    };
+
+    const openBlockUserConfirmationModal = () => {
+        setShowBlockUserConfirmationModal(true);
+    };
+
+    const openChargeModal = () => {
+        setShowChargeModal(true);
+    };
+
+    const closeProfileModal = () => {
+        setShowProfileModal(false);
+    };
+
+    const closeBlockUserConfirmationModal = () => {
+        setShowBlockUserConfirmationModal(false);
+    };
+
+    const closeChargeModal = () => {
+        setShowChargeModal(false);
+    };
+
+    function handleAmountChange(event) {
+        setAmountToCharge(event.target.value);
+    }
 
     function mapRole(role) {
         const roles = [['guest', 'Usuario'], ['host', 'Anfitrión'], ['admin', 'Administrador']]
@@ -16,10 +52,42 @@ export default function UserInfo({user}) {
             <div className='name'>{user.name} {user.surname}</div>
             <div className='role'>{mapRole(user.role)}</div>
             <div className='options'>
-                <Link className='option-profile'>Ver perfil</Link>
-                <Link className='option-block'>Bloquear usuario</Link>
-                <Link className='option-money'>Cargar saldo</Link>
+                <Link className='option-profile' onClick={openProfileModal}>Ver perfil</Link>
+                <Link className='option-block' onClick={openBlockUserConfirmationModal}>Bloquear usuario</Link>
+                <Link className='option-money' onClick={openChargeModal}>Cargar saldo</Link>
             </div>
+            <Modal
+                open={showProfileModal}
+                onClose={closeProfileModal}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description">
+                {user.name} {user.surname}
+                {user.role}
+                {user.email}
+                {user.city}
+                {user.phone}
+            </Modal>
+            <Modal
+                open={showBlockUserConfirmationModal}
+                onClose={closeBlockUserConfirmationModal}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description">
+                Estás seguro que deseas bloquear al usuario {user.name} {user.surname}?
+                <div className='confirmation-buttons'>
+                    <Button className="block-user-no-button" variant="contained" onClick={closeBlockUserConfirmationModal}>No</Button>
+                    <Button className="block-user-yes-button" variant="contained" onClick={closeBlockUserConfirmationModal}>Sí</Button>
+                </div>
+            </Modal>
+            <Modal
+                open={showChargeModal}
+                onClose={closeChargeModal}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description">
+                Ingrese monto a cargar a {user.name} {user.surname}
+                <TextField id="amount" variant="outlined"
+                           onChange={handleAmountChange} />
+                <Button className="charge-button" variant="contained" onClick={closeChargeModal}>Cargar saldo</Button>
+            </Modal>
         </div>
     )
 }
